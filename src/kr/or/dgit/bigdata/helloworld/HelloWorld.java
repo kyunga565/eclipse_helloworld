5834a9052e913bb67287d72a1e94f595a6466f0f
kyunga <dnldzmdnldzm@naver.com>
2016-12-13 14:54:48 +0900

    의도적인 한줄추가하여 충돌발생시킬거 **

--- src/kr/or/dgit/bigdata/helloworld/HelloWorld.java
+++ src/kr/or/dgit/bigdata/helloworld/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 		System.out.println("HotFix에서 작업");
+		System.out.println("의도적으로 충돌작업");
 	}
 }

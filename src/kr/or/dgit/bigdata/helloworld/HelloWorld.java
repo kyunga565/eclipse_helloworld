e9859de0b761dba877397aee6f6380ec33cad3d5
kyunga <dnldzmdnldzm@naver.com>
2016-12-13 14:51:05 +0900

    HelloWorld.java 한줄추가

--- src/kr/or/dgit/bigdata/helloworld/HelloWorld.java
+++ src/kr/or/dgit/bigdata/helloworld/HelloWorld.java
@@ -3,5 +3,6 @@ package kr.or.dgit.bigdata.helloworld;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
+		System.out.println("HotFix에서 작업");
 	}
 }

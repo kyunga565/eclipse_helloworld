98fecd7f4e15342fb993550bce3a840c100d94e1
kyunga <dnldzmdnldzm@naver.com>
2016-12-13 15:21:12 +0900

    한줄추가

--- src/kr/or/dgit/bigdata/helloworld/PushPrint.java
+++ src/kr/or/dgit/bigdata/helloworld/PushPrint.java
@@ -3,5 +3,6 @@ package kr.or.dgit.bigdata.helloworld;
 public class PushPrint {
 	public static void main(String[] args) {
 		System.out.println("Push github");
+		System.out.println("Local에서 한줄추가");
 	}
 }
